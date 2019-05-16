cd1a26a264eaa34edf4163268ee5df3eb90f2887
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-16 13:11:01 +0200

    Add createTweet function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-var url = 'http://api.icndb.com/jokes/random';
-var paragraph = document.getElementById('joke');
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
-});
 
-function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-      var response = JSON.parse(xhr.response);
-      paragraph.innerHTML = response.value.joke;
-    });
-    xhr.send();
-  }
+function getQuote() {
+    fetch(quoteUrl, { cache: "no-store" })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(createTweet);
+}
 
-  window.onload = getJoke;
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
+}
+
