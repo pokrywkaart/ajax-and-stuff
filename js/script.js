e4cad0c65e00bbbf36931a9cae71488cc0fcb625
Artur Pokrywka <pokrywkaart@gmail.com>
2019-06-10 13:53:12 +0200

    Creating new branch

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
-var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = document.getElementById('countries');
+const url = 'https://restcountries.eu/rest/v2/name/';
+const countriesList = document.getElementById('countries');
 
 document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
-    var countryName = document.getElementById('country-name').value;
+    let countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
     fetch(url + countryName)
         .then(function(resp) {
             return resp.json();
         })
+        // .then(doFilter)
         .then(showCountriesList);
-}
+};
 
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item){
-        var liEl = document.createElement('li');
-        liEl.innerText = item.name + ", " + "Capital: " + item.capital;
+        const liEl = document.createElement('li');
+        liEl.innerText = item.name + ', ' + "Capital: " + item.capital;
         countriesList.appendChild(liEl);
     });
-}
+};
+
+console.log(countriesList);
+
+// function doFilter(countriesList) { 
+//     if (countriesList.length > 1) {
+//         return countriesList[1];
+//     }
+// };
